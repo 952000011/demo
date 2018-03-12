827684bcd0bbff1785446d432b01d05b6777ca00
I <I@LAPTOP-7LHV6MFQ>
2018-03-12 17:01:01 +0800

    2222

--- src/sssss/asdasd.java
+++ src/sssss/asdasd.java
@@ -2,7 +2,9 @@ package sssss;
 
 public class asdasd {
 	public static void main(String[] args) {
-		System.out.println("2343242342342342342342");	}
+		System.out.println("2343242342342342342342");	
+		System.out.println("111111111");
+	}
 	
 
 }
